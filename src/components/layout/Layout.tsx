5adfa81a63bc4bba2bcfa83a76771bc2094c7c13
aleks <aleksiya.solovyova@student.kdg.be>
2025-12-01 09:25:35 +0100

    .

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -32,7 +32,7 @@ export function Layout() {
             {/*    />*/}
             {/*</div>*/}
             <NavBar />
-            <main className="w-full px-4 sm:px-6 lg:px-8 py-8 pointer-events-none">
+            <main className="w-full px-4 sm:px-6 lg:px-8 py-8">
                 <Outlet />
             </main>
         </div>
