2309422a80be1f8b523ebfd82a1085772727d70a
aleks <aleksiya.solovyova@student.kdg.be>
2025-12-22 02:10:00 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from '@/components/layout/Layout';
-import { GamesPage } from '@/pages/GamesPage';
-import { FriendsPage } from '@/pages/FriendsPage';
-import {AchievementsPage} from "@/pages/AchievementsPage.tsx";
+import { GamesPage } from '@/pages/player/GamesPage.tsx';
+import { FriendsPage } from '@/pages/player/FriendsPage.tsx';
+import {AchievementsPage} from "@/pages/player/AchievementsPage.tsx";
 import {KeycloakProvider} from "@/context/KeycloakProvider.tsx";
 import {AdminDashboardPage} from "@/pages/admin/AdminDashboardPage.tsx";
 import {CreateGamePage} from "@/pages/developer/CreateGamePage.tsx";
